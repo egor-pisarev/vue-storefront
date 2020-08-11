3355c7bdc836dd298b70fd0a243686e07fb1d481
egor.pisarev <egor.pisarev@gmail.com>
2020-08-11 16:40:50 +0400

    translate

--- src/modules/payment-cash-on-delivery/components/Info.vue
+++ src/modules/payment-cash-on-delivery/components/Info.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="mb15 mt20 payment-cash-on-delivery-info-container">
     <h4 class="mt0">
-      Payment Information
+      Информация об оплате
     </h4>
     <div class="bg-cl-secondary px20 py20">
       <p>
-        You are going to pay for this order upon delivery.
+        Вы должны будете заплатить за заказ при получении.
       </p>
     </div>
   </div>
